dc8029aff10d23a02af91b0dee1b5fa55dd38898
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2008-07-21 16:47:44 +0200

    Rewrote the method generator using define_method

--- lib/validates_as_time.rb
+++ lib/validates_as_time.rb
@@ -18,30 +18,32 @@ module ActiveRecord
       def validates_as_time(*attr_names)
         configuration = ValidatesAsTime.default_configuration.merge(attr_names.extract_options!)
         attr_names.each do |attr_name|
-          class_eval(<<-EOS, __FILE__, __LINE__)
-            def #{attr_name}_string
-              return @_#{attr_name}_string if @_#{attr_name}_string
-              c = #{attr_name} || (Object.const_defined?(:Chronic) ? Chronic.parse("#{configuration[:default]}") : Time.parse("#{configuration[:default]}"))
-              c.strftime("#{configuration[:format]}") if c
-            end
+          define_method("#{attr_name}_string") do
+            return str if str = instance_variable_get("@_#{attr_name}_string")
 
-            def #{attr_name}_string=(str)
-              @_#{attr_name}_string = str
+            c = send(attr_name) || (Object.const_defined?(:Chronic) ? Chronic.parse(configuration[:default]) : Time.parse(configuration[:default]))
+            c.strftime(configuration[:format]) if c
+          end
+
+          define_method("#{attr_name}_string=") do |str|
+            begin
+              instance_variable_set("@_#{attr_name}_string", str)
+
               if str.blank?
-                self.#{attr_name} = nil
+                write_attribute(attr_name, nil)
               else
                 if Object.const_defined?(:Chronic)
                   c = Chronic.parse(str)
                   raise ArgumentError if c.nil?
-                  self.#{attr_name} = c
+                  write_attribute(attr_name, c)
                 else
-                  self.#{attr_name} = Time.parse(str)
+                  write_attribute(attr_name, Time.parse(str))
                 end
               end
             rescue ArgumentError
-              @_#{attr_name}_invalid = true
+              instance_variable_set("@_#{attr_name}_invalid", true)
             end
-          EOS
+          end
 
           validates_each attr_name do |record, attr, value|
             if record.instance_variable_defined?("@_#{attr_name}_invalid") && record.instance_variable_get("@_#{attr_name}_invalid")
